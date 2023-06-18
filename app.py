4de1e670d2225b36b4f049cf86768f3f7305a508
lostab <lostab@gmail.com>
2023-06-18 15:23:10 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import bottle
 import os
 import random
+import json
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
  
@@ -10,31 +11,37 @@ root_dir = os.path.dirname(os.path.realpath(__file__))
 def main():
   return bottle.static_file('app.html', root=root_dir)
 
+@bottle.route('/favicon.ico')
+def icon():
+  return bottle.static_file('music.ico', root=root_dir)
+
 @bottle.route('/m/:m#.*#')
 def music(m):
+    q = bottle.request.query.q.strip()
     musics = []
     for music in os.listdir(root_dir + '/music'):
         if music.endswith('.mp3'):
-            musics.append(music)
-    random_music = musics[random.randint(0, len(musics) - 1)]
-    if not m:
-        print(random_music)
-        return bottle.static_file(random_music, root=root_dir + '/music')
-    else:
-        searchs = m.split(' ')
-        results = []
-        for music in musics:
-            fit = True
-            for search in searchs:
-                if search in music and music not in results:
-                    results.append(music)
-                else:
-                    fit = False
-            if fit:
-                return bottle.static_file(music, root=root_dir + '/music')
-        if results:
-            return bottle.static_file(results[0], root=root_dir + '/music')
+            if not q or q in music:
+                musics.append(music)
+    if musics:
+        random_id = random.randint(0, len(musics) - 1)
+        random_music = musics[random_id]
+        if not m:
+            result = {
+                'id': random_id,
+                'music': random_music.rstrip('.mp3')
+            }
+            return json.dumps(result, ensure_ascii=False)
         else:
-            return bottle.static_file(random_music, root=root_dir + '/music')
+            if len(m.split('?')) > 1:
+                m = m.split('?')[0]
+            music_id  = int(m)
+            if music_id < len(musics):
+                music = musics[music_id]
+                return bottle.static_file(music, root=root_dir + '/music')
+            else:
+                return None
+    else:
+        return None
 
-bottle.run(host='127.0.0.1', port=8787, reloader = True)
+bottle.run(host='0.0.0.0', port=8787, reloader=True)
